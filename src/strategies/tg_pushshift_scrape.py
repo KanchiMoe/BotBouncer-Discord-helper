d32196e125fcd76402307a5aeca31c68d0bf4217
KanchiMoe <github@chiyo-chan.me>
2025-09-14 19:17:41 +0100

    better debugging logging for tg scrape

--- src/strategies/tg_pushshift_scrape.py
+++ src/strategies/tg_pushshift_scrape.py
@@ -4,6 +4,7 @@ from src.pushshift.query_pushshift import get_posts
 from src.helpers.extract_subreddits import extract_subreddit_post_pushshift
 
 def entry(telegram_handle):
+    logging.info("Searching pushshift for posts containing the TG handle: %s", telegram_handle)
     ps_data = get_posts(telegram_handle)
 
     # extract the subreddits
