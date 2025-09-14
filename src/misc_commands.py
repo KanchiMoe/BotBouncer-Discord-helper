dbcfb672c7bee2e5626e7f4d70bf53458db7c6c2
KanchiMoe <5897898+KanchiMoe@users.noreply.github.com>
2025-09-14 18:22:29 +0100

    start from scratch (#1)

--- src/misc_commands.py
+++
@@ -1,8 +0,0 @@
-import discord
-from discord import app_commands
-from src.discordbot import BOT
-
-#/ping
-@app_commands.command(name="ping", description="Ping")
-async def ping_slash(interaction: discord.Interaction):
-    await interaction.response.send_message(":stopwatch: | pong!")
