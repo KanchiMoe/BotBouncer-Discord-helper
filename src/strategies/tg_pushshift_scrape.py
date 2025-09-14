ba9c98fb28bbf057b605e28bdb8aad4162f026e4
KanchiMoe <github@chiyo-chan.me>
2025-09-14 21:39:31 +0100

    expand tg scrape

--- src/strategies/tg_pushshift_scrape.py
+++ src/strategies/tg_pushshift_scrape.py
@@ -7,5 +7,14 @@ def entry(telegram_handle):
     logging.info("Searching pushshift for posts containing the TG handle: %s", telegram_handle)
     ps_data = get_posts(telegram_handle)
 
+    # how many items did we find
+    ps_item_count = len(ps_data)
+
+    # handle if 0 items
+    if ps_item_count == 0:
+        logging.warning("0 posts were found by Pushshift.")
+        return None
+    logging.info(f"{ps_item_count} posts were found by Pushshift")
+
     # extract the subreddits
     extract_subreddit_post_pushshift(ps_data)
