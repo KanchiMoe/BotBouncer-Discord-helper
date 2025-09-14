e3b31f7114dde565991e0807500c0792a39599e0
KanchiMoe <github@chiyo-chan.me>
2025-09-14 18:55:08 +0100

    add stragy to scrape pushshift for TG handles

---
+++ src/strategies/tg_pushshift_scrape.py
@@ -0,0 +1,10 @@
+import logging
+
+from src.pushshift.query_pushshift import get_posts
+from src.helpers.extract_subreddits import extract_subreddit_post_pushshift
+
+def entry(telegram_handle):
+    ps_data = get_posts(telegram_handle)
+
+    # extract the subreddits
+    extract_subreddit_post_pushshift(ps_data)
