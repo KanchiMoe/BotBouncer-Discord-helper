c62a95e2fbf933023b0409b87a6d9a30842eeedd
KanchiMoe <github@chiyo-chan.me>
2025-09-17 19:04:12 +0100

    revert as i found a hack

--- src/reddit/__init__.py
+++ src/reddit/__init__.py
@@ -1,1 +1,6 @@
-from .shadowban_checker import shadowban_check
+from .shadowban_check import shadowban_check as _shadowban_check
+
+from functools import wraps
+@wraps(_shadowban_check)
+def shadowban_check(*args, **kwargs):
+    return _shadowban_check(*args, **kwargs)
